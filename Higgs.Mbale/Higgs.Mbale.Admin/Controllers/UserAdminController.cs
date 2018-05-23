b5aa802fb4d14bfaceb2acfdac8afa5af9ea8772
semakulajohn <jpolosemakula@gmail.com>
2018-05-23 11:28:55 +0300

    added new web project
    removed old webproject called mbale.admin and added new one called
    mbale.web

---
+++ Higgs.Mbale/Higgs.Mbale.Admin/Controllers/UserAdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Higgs.Mbale.Admin.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        //
+        // GET: /UserAdmin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
