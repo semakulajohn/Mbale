b6e4dd6e580d1105f02f47709984152f88e5d3d5
semakulajohn <jpolosemakula@gmail.com>
2018-05-26 09:27:20 +0300

    changed REMS
    Changed REMS TO MIGM

--- Higgs.Mbale/Higgs.Mbale.Web/app/scripts/directives/directives.js
+++ Higgs.Mbale/Higgs.Mbale.Web/app/scripts/directives/directives.js
@@ -19,9 +19,9 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title
-                var title = 'REMS';
+                var title = ' MIGM';
                 // Create your own title pattern
-                if (toState.data && toState.data.pageTitle) title = toState.data.pageTitle + ' - REMS';
+                if (toState.data && toState.data.pageTitle) title = toState.data.pageTitle + ' -  MIGM';
                 $timeout(function() {
                     element.text(title);
                 });
